3dfa0544e5ad707d0bf3e52f5fa735f2676bc27e
Cecilia <cecilia.perdomo@gmail.com>
2023-07-07 17:12:25 -0300

    Caja Diaria

--- src/front/js/component/caja_diaria/resumen_caja.jsx
+++ src/front/js/component/caja_diaria/resumen_caja.jsx
@@ -1,18 +1,16 @@
 import React, { useContext, useEffect, useState } from "react";
 import { Context } from "../../store/appContext";
-import { Link } from "react-router-dom";
 import { toast, ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import { format } from 'fecha'
-
+import moment from "moment";
 
 export const ResumenCajaDiaria = () => {
     const { store, actions } = useContext(Context)
-    
+
     useEffect(() => {
         actions.obtenerMovimientosCajaDiaria();
-      }, []);
-    
+    }, []);
+
 
     return (
         <>
@@ -22,42 +20,39 @@ export const ResumenCajaDiaria = () => {
                 <br />
 
                 <div>
-                <table className="table" style={{ color: "white" }}>
-                                <thead>
-                                    <tr>
-                                        <th scope="col">Fecha</th>
-                                        <th scope="col" className="text-center">Cantidad de clientes</th>
-                                        <th scope="col" className="text-center">Total Ingresos (Mensualidades)</th>
-                                        <th scope="col" className="text-center">Total Ingresos (Ventas)</th>
-                                        <th scope="col" className="text-center">Total Egresos</th>
-                                        <th scope="col" className="text-center">Total</th>
-                                        <th scope="col">Observaciones</th>
-                                    </tr>
-                                </thead>
-                                
-                                <tbody>
-                                    {store.cajaDiaria.map((item, id) => ( 
-                                        <tr key={id}>
-                                            <td>{item.fecha}</td>
-                                            <td className="text-center">{item.cantidadalumnos}</td>
-                                            <td className="text-center">$ {item.totalmensualidades}</td>
-                                            <td className="text-center">$ {item.totalventas}</td>
-                                            <td className="text-center">$ {item.totalpagoprov}</td>
-                                            <td className="text-center" 
-                                            style={{color: "red"}}>
-                                                $ {(item.totalmensualidades + item.totalventas) - item.totalpagoprov}
-                                            </td>
-                                            <td>{item.observaciones}</td>
-                                            
-                                        </tr>
-                                    ))}
-                                </tbody>
-                            </table>
+                    <table className="table" style={{ color: "white" }}>
+                        <thead>
+                            <tr>
+                                <th scope="col">Fecha</th>
+                                <th scope="col" className="text-center">Cantidad de clientes</th>
+                                <th scope="col" className="text-center">Total Ingresos (Mensualidades)</th>
+                                <th scope="col" className="text-center">Total Ingresos (Ventas)</th>
+                                <th scope="col" className="text-center">Total Egresos</th>
+                                <th scope="col" className="text-center">Total</th>
+                            </tr>
+                        </thead>
+
+                        <tbody>
+                            {store.cajaDiaria.map((item, id) => (
+                                <tr key={id}>
+                                    <td>{item.fecha}</td>
+                                    <td className="text-center">{item.cantidadalumnos}</td>
+                                    <td className="text-center">$ {item.totalmensualidades}</td>
+                                    <td className="text-center">$ {item.totalventas}</td>
+                                    <td className="text-center">$ {item.totalpagoprov}</td>
+                                    <td className="text-center"
+                                        style={{ color: "red" }}>
+                                        $ {(item.totalmensualidades + item.totalventas) - item.totalpagoprov}
+                                    </td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
                 </div>
-            
+
             </div>
             <ToastContainer />
             <br />
-        </> 
+        </>
     )
 }

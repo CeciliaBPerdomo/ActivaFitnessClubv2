8bec776f1dde22781b9519780dfdbec4f3153fc8
Cecilia Perdomo <cecilia.perdomo@gmail.com>
2024-05-28 15:26:18 -0300

    Store en Actualizar datos del alumno

--- src/front/js/pages/HomeAlumno/actualizarDatos.js
+++ src/front/js/pages/HomeAlumno/actualizarDatos.js
@@ -1,6 +1,9 @@
-import React from "react";
+import React, { useContext, useEffect } from "react"; 
+import { Context } from "../../store/appContext";
 
 const ActualizarDatos = () => {
+    const { actions, store } = useContext(Context)
+    
     return (
         <>
             {/* // <!-- Button trigger modal --> */}
